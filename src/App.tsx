3b4c415dda12cd96e0032e0d4d7e54a13e143262
solalcaraz <alcarazmsol@gmail.com>
2025-11-01 23:42:12 -0300

    probando estilo de chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { Button } from '@chakra-ui/react'
 import './App.css'
 
 import { AlgoQuePedirRouter } from './routes' 
 
 const App = () => {
   return (
-      <div className="App">
-        <AlgoQuePedirRouter />
-      </div>
+      // <div className="App">
+      //   <AlgoQuePedirRouter />
+      // </div>
+    <div>
+      <h3>Chakra hola hola</h3>
+      <Button>Boton</Button>
+    </div>
   )
 }
 
