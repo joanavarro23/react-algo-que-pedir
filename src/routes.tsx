b251d53f9636b64d3c6afe73e0d2fd57139c38d3
solalcaraz <alcarazmsol@gmail.com>
2025-11-01 23:32:24 -0300

    configuro app y agrego archivo de rutas

---
+++ src/routes.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+export const AQPRoutes = () =>
+    <Routes>
+        {/* <Route path="/" element={< />} /> */}
+        {/* <Route path="/usuarios" element={< />} /> */}
+    </Routes>
+
+export const AlgoQuePedirRouter = () =>
+    <Router>
+        <AQPRoutes />
+    </Router>
