420fe0f77d7f2700dcc4d8ce577aadaee1b38cea
solalcaraz <alcarazmsol@gmail.com>
2025-11-03 21:12:55 -0300

    fix de la ruta para no forzarlo

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 export const AQPRoutes = () =>
     <Routes>
-        <Route path="/" element={<PerfilUsuario/>} />
+        <Route path="/perfilUsuario" element={<PerfilUsuario/>} />
     </Routes>
 
 export const AlgoQuePedirRouter = () =>
