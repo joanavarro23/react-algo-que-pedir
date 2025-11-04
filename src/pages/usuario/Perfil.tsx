9f7386a5942578230d667a1ac3dd692a8f329ec3
solalcaraz <alcarazmsol@gmail.com>
2025-11-03 21:58:21 -0300

    mejoras en el maquetado del perfil

--- src/pages/usuario/Perfil.tsx
+++ src/pages/usuario/Perfil.tsx
@@ -3,59 +3,68 @@ import { Avatar, Card, Field, Heading, Input, Stack, Text, VStack } from '@chakr
 
 export const PerfilUsuario = () => {
     return(
-        <Stack>
+        <Stack py='5'>
+           <Heading as='h1' size='md' textAlign="center">Perfil</Heading>
             {/* Preview de la informacion del usuario */}
-           <Heading as='h1'>Perfil</Heading>
-           <VStack>
-                <Avatar.Root>
+           <VStack py='5'>
+                <Avatar.Root size='2xl'>
                     <Avatar.Image src='/usuario-chica.png'/>
                 </Avatar.Root> 
-                <Heading as='h2'>Nombre</Heading>
-                <Text color='parrafos'>Email</Text>
+                <Heading as='h2' size='2xl'>Nombre Apellido</Heading>
+                <Text color='parrafos'>email@gmail.com</Text>
            </VStack>
+
            {/* Informacion personal */}
-           <Card.Root>
+           <Card.Root variant='subtle'>
                 <Card.Header>
                     <Card.Title>Informacion Personal</Card.Title>
                 </Card.Header>
                 <Card.Body>
-                    <Stack>
+                    <Stack gap='4'>
                         <Field.Root>
                             <Field.Label>Nombre</Field.Label>
-                            <Input />
+                            <Input placeholder='Nombre'/>
                         </Field.Root>
                         <Field.Root>
                             <Field.Label>Apellido</Field.Label>
-                            <Input />
+                            <Input placeholder='Apellido'/>
                         </Field.Root>
                         <Field.Root>
                             <Field.Label>Dirección</Field.Label>
-                            <Input />
+                            <Input placeholder='Dirección'/>
                         </Field.Root>
                         <Field.Root>
                             <Field.Label>Ubicación</Field.Label>
-                            <Input />
+                            <Input placeholder='Ciudad, Provincia'/>
                         </Field.Root>
                         <Stack direction='row'>
                             <Field.Root>
                                 <Field.Label>Latitud</Field.Label>
-                                <Input type='number'/>
+                                <Input type='number' placeholder='Ej: -34.61'/>
                             </Field.Root>
                             <Field.Root>
                                 <Field.Label>Longitud</Field.Label>
-                                <Input type='number'/>
+                                <Input type='number' placeholder='Ej: 58.38'/>
                             </Field.Root>
                         </Stack>
                     </Stack>
                 </Card.Body>
            </Card.Root>
+
            {/* Preferencias */}
-           <Stack>
-                <Heading as='h2'>Preferencias</Heading>
-                <Text>Criterios de búsqueda</Text>
-                <Text>Ingredientes preferidos</Text>
-                <Text>Ingredientes a evitar</Text>
-           </Stack>
+           <Card.Root variant='subtle'>
+                <Card.Header>
+                    <Card.Title>Preferencias</Card.Title>
+                </Card.Header>
+                <Card.Body>
+                    <Stack>
+                        <Text>Criterios de búsqueda</Text>
+                        <Text>Ingredientes preferidos</Text>
+                        <Text>Ingredientes a evitar</Text>
+                    </Stack>
+                </Card.Body>
+           </Card.Root>
+
            <Button>Guardar Cambios</Button>
         </Stack>
     )
