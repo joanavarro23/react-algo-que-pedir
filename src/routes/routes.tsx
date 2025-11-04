44557d2b679f55335f6e265009fd4d9544fc7049
Joaquín Navarro <joanavarro23@hotmail.com>
2025-11-04 02:13:53 -0300

    agrego ruta de checkout en routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,12 @@
 // RUTAS DE LA PAGINA [React Routes]
+import { CheckoutPedido } from '@/pages/checkout-pedido/Checkout'
 import { PerfilUsuario } from '@/pages/usuario/Perfil'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 export const AQPRoutes = () =>
     <Routes>
         <Route path="/" element={<PerfilUsuario/>} />
+        <Route path="/checkout-pedido" element={<CheckoutPedido/>}> </Route>
     </Routes>
 
 export const AlgoQuePedirRouter = () =>
