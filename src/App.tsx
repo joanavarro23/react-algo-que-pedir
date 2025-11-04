3d96c781e60b13944fc335631aeff2ce8260e3ad
solalcaraz <alcarazmsol@gmail.com>
2025-11-04 09:21:23 -0300

    se agrega el toast a la app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Box } from '@chakra-ui/react'
 import { AlgoQuePedirRouter } from './routes/routes'
+import { Toaster } from './components/ui/toaster'
 
 const App = () => {
   return (
     <Box maxW="400px" mx="auto" w="100%" border='1px solid'>
       <AlgoQuePedirRouter />
+      <Toaster />
     </Box>
   )
 }
