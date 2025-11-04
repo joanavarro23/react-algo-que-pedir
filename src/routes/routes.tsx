909139c08cd6e13fb9ddfe62fabac0bca5ac87bc
solalcaraz <alcarazmsol@gmail.com>
2025-11-03 23:17:40 -0300

    agrego archivos de las otras vistas criterios e ingredinete

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 export const AQPRoutes = () =>
     <Routes>
         <Route path="/perfilUsuario" element={<PerfilUsuario/>} />
+        {/* <Route path="/criteriosBusqueda" element={<CriteriosBusqueda />} /> */}
+        {/* <Route path="/ingredinetes/:id" element={<Ingredientes />} /> */}
     </Routes>
 
 export const AlgoQuePedirRouter = () =>
