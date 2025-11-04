f8254a0a5a24a14abfd7f50845cfa62c8ed2f721
solalcaraz <alcarazmsol@gmail.com>
2025-11-03 22:17:55 -0300

    agrego como en el ejemplo de dodino

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from './styles/theme.ts'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <ChakraProvider value={theme} >
       <App />
     </ChakraProvider>
-  </StrictMode>,
+  </React.StrictMode>,
 )
