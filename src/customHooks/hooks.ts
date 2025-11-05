7f3a4d3869d9512de6927fe5d5ff45d2ba31639b
solalcaraz <alcarazmsol@gmail.com>
2025-11-05 11:53:15 -0300

    renombro archivo hook a useOnInit

--- src/customHooks/hooks.ts
+++
@@ -1,7 +0,0 @@
-import { useEffect } from 'react'
-
-export const useOnInit = (initialCallBack: () => void) => {
-    useEffect(() => {
-      initialCallBack()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])}
