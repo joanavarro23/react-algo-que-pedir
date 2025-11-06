35a88aa7d45266c07363ae0aef937e653c792c27
solalcaraz <alcarazmsol@gmail.com>
2025-11-06 12:02:09 -0300

    maquetado de criterios

--- src/pages/usuario/preferencias/criteriosBusqueda.tsx
+++ src/pages/usuario/preferencias/criteriosBusqueda.tsx
@@ -1,7 +1,42 @@
-import { Heading } from '@chakra-ui/react'
+import { Button } from '@/components/boton/boton'
+import { criterios } from '@/pages/usuario/preferencias/criterios'
+import { Badge, CheckboxCard, CheckboxGroup, Flex, Heading, HStack, IconButton, Stack } from '@chakra-ui/react'
+import { IoMdArrowBack } from 'react-icons/io'
 
 export const CriteriosBusqueda = () => {
     return (
-        <Heading as='h1'>Estas en la vista de criterios de busqueda</Heading>
+        <Stack py='5'>
+            <HStack alignItems='center' justifyContent='center'>
+                <IconButton variant="ghost"><IoMdArrowBack/></IconButton>
+                <Heading as='h1'>Selecciona tu criterio</Heading>
+            </HStack>
+
+            <CheckboxGroup>
+                <Flex gap="2" direction='column'>
+                    {criterios.map((item) => (
+                        <CheckboxCard.Root key={item.value} value={item.value} >
+                            <CheckboxCard.HiddenInput />
+                            <CheckboxCard.Control >
+                                <CheckboxCard.Content>
+                                    <CheckboxCard.Label>{item.title}</CheckboxCard.Label>
+                                    <CheckboxCard.Description>
+                                        {item.description}
+                                    </CheckboxCard.Description>
+                                </CheckboxCard.Content>
+                                <CheckboxCard.Indicator />
+                            </CheckboxCard.Control>
+                            <CheckboxCard.Addon>
+                                <HStack>
+                                Some supporting text
+                                <Badge variant="solid">New</Badge>
+                                </HStack>
+                            </CheckboxCard.Addon>
+                        </CheckboxCard.Root>
+                    ))}
+                </Flex>
+            </CheckboxGroup>
+
+            <Button>Guardar</Button>
+        </Stack>
     )
 }
