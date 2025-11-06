0b27e891f1a1ee6280a3a9470566fe67c9764619
solalcaraz <alcarazmsol@gmail.com>
2025-11-06 10:56:36 -0300

    sub rutas del perfil por fuera del perfil-usuario

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,21 +2,23 @@
 import { CheckoutPedido } from '@/pages/checkout-pedido/Checkout'
 import { PerfilUsuario } from '@/pages/usuario/Perfil'
 import { CriteriosBusqueda } from '@/pages/usuario/preferencias/criteriosBusqueda'
-import { Ingredientes } from '@/pages/usuario/preferencias/ingredientes'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { IngredientesEvitar, IngredientesPreferidos } from '@/pages/usuario/preferencias/ingredientes'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 import { AppLayout } from '@/components/layout/AppLayout'
 import { DetalleLocal } from '@/pages/detalle-local/DetalleLocal'
 
 export const AQPRoutes = () =>
     <Routes>
-        /*El layout envuelve al resto de las paginas*/
+        {/* El layout envuelve al resto de las paginas*/ }
         <Route path="/" element={ <AppLayout /> }>
-            <Route path="/perfil-usuario" element={<PerfilUsuario/>} />
-            <Route path="/perfilUsuario/preferencias/criteriosBusqueda" element={<CriteriosBusqueda />} />
-            <Route path="/perfilUsuario/preferencias/ingredientes" element={<Ingredientes />} />
+            <Route path="/perfil-usuario" element={ <PerfilUsuario/> }> {/* ACA IRIAN LAS SUBRUTAS DEL PERFIL */}</Route>
+            <Route path="criterios-busqueda" element={<CriteriosBusqueda />} />
+            <Route path="ingredientes-preferidos" element={<IngredientesPreferidos />} />
+            <Route path="ingredientes-evitar" element={<IngredientesEvitar />} />
             <Route path="/checkout-pedido" element={<CheckoutPedido/>}> </Route>
             <Route path="/crear-pedido" element={<DetalleLocal/>} />
         </Route>
+        <Route path="*" element={ <Navigate to={'/'}/> } />
     </Routes>
 
 export const AlgoQuePedirRouter = () =>
