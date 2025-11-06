8d3f13a0d457e1fe0739574b09e45bacbc725f1a
solalcaraz <alcarazmsol@gmail.com>
2025-11-06 12:02:23 -0300

    criterios con sus detalles en una lista

---
+++ src/pages/usuario/preferencias/criterios.ts
@@ -0,0 +1,8 @@
+export const criterios = [
+    { value: 'vegano', title: 'Veganos', description: 'Sólo los platos veganos' },
+    { value: 'exquisito', title: 'Exquisitos', description: 'Sólo platos de autor' },
+    { value: 'conservador', title: 'Conservadores', description: 'Sólo platos con ingredientes preferidos' },
+    { value: 'fiel', title: 'Fieles', description: 'Sólo los restaurantes preferidos' },
+    { value: 'marketing', title: 'Marketing', description: 'Filtran platos por palabras buscadas' },
+    { value: 'impaciente', title: 'Impacientes', description: 'Dentro de una distancia máxima' },
+]
