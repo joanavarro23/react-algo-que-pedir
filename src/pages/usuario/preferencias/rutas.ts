a3de2e925d2e4a6b25b2a5602675a7f610c95f9f
solalcaraz <alcarazmsol@gmail.com>
2025-11-06 10:57:11 -0300

    fix de la lista de las rutas pero no funciona (martes 4)

--- src/pages/usuario/preferencias/rutas.ts
+++ src/pages/usuario/preferencias/rutas.ts
@@ -1,5 +1,8 @@
-export const preferencias = [
-    { label: 'Criterios de búsqueda', path: '/perfilUsuario/preferencias/criteriosBusqueda' },
-    { label: 'Ingredientes preferidos', path: '/perfilUsuario/preferencias/ingredientes' },
-    { label: 'Ingredientes a evitar', path: '/perfilUsuario/preferencias/ingredientes' },
+export const preferencias: {
+    label: string
+    path: 'criteriosBusqueda' | 'ingredientes-preferidos' | 'ingredientes-evitar'
+}[] = [
+    { label: 'Criterios de búsqueda', path: 'criteriosBusqueda' },
+    { label: 'Ingredientes preferidos', path: 'ingredientes-preferidos' },
+    { label: 'Ingredientes a evitar', path: 'ingredientes-evitar' },
 ]
