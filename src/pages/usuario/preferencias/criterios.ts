3f56ddf308b5c087a0ab29b3b9fb26a95f61b6d7
solalcaraz <alcarazmsol@gmail.com>
2025-11-06 16:05:06 -0300

    maquetado criterios y componte row

--- src/pages/usuario/preferencias/criterios.ts
+++ src/pages/usuario/preferencias/criterios.ts
@@ -1,13 +1,25 @@
-export const criterios = [
-    { value: 'vegano', title: 'Veganos', description: 'Sólo los platos veganos', extra: false },
-    { value: 'exquisito', title: 'Exquisitos', description: 'Sólo platos de autor', extra: false },
-    { value: 'conservador', title: 'Conservadores', description: 'Sólo platos con ingredientes preferidos', extra: false},
-    { value: 'fiel', title: 'Fieles', description: 'Sólo los restaurantes preferidos', extra: true},
-    // { value: 'marketing', title: 'Marketing', description: 'Filtran platos por palabras buscadas', extra: false},
-    // { value: 'impaciente', title: 'Impacientes', description: 'Dentro de una distancia máxima', extra: false },
-]
+import type { ReactNode } from 'react'
 
-export const restaurantes = [
+const restaurantes = [
     { id: 1, nombre: 'La Pizzería', detalles: '4 ★ • 25 - 35 min • $' },
     { id: 2, nombre: 'El Gran Sabor', detalles: '4 ★ • 20 - 40 min • $$' },
+]
+const palabras = [
+    { id: 1, value: 'ajillo', nombre: 'Ajillo' },
+    { id: 2, value: 'oleo', nombre: 'Óleo' }
+]
+
+export const criterios: {
+    value: string
+    titulo: string
+    descripcion: string
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    items?: any[]
+}[] = [
+    { value: 'vegano', titulo: 'Veganos', descripcion: 'Sólo los platos veganos' },
+    { value: 'exquisito', titulo: 'Exquisitos', descripcion: 'Sólo platos de autor' },
+    { value: 'conservador', titulo: 'Conservadores', descripcion: 'Sólo platos con ingredientes preferidos' },
+    { value: 'fiel', titulo: 'Fieles', descripcion: 'Sólo los restaurantes preferidos', items: restaurantes},
+    { value: 'marketing', titulo: 'Marketing', descripcion: 'Filtran platos por palabras buscadas', items: palabras} ,
+    { value: 'impaciente', titulo: 'Impacientes', descripcion: 'Dentro de una distancia máxima' },
 ]
--- src/pages/usuario/preferencias/criteriosBusqueda.tsx
+++ src/pages/usuario/preferencias/criteriosBusqueda.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/boton/boton'
-import { criterios, restaurantes } from '@/pages/usuario/preferencias/criterios'
+import { ItemRow } from '@/components/itemRow/itemRow'
+import { criterios } from '@/pages/usuario/preferencias/criterios'
 import { CheckboxCard, CheckboxGroup, Heading, HStack, IconButton, Stack, Text } from '@chakra-ui/react'
 import { IoMdArrowBack } from 'react-icons/io'
 import { MdClose } from 'react-icons/md'
@@ -14,51 +15,28 @@ export const CriteriosBusqueda = () => {
 
             <CheckboxGroup>
                 <Stack gap="2">
-                    {criterios.map((item) => (
-                        <CheckboxCard.Root key={item.value} value={item.value} >
+                    {criterios.map((criterio) => (
+                        <CheckboxCard.Root key={criterio.value} value={criterio.value} >
                             <CheckboxCard.HiddenInput />
                             <CheckboxCard.Control >
                                 <CheckboxCard.Content>
-                                    <CheckboxCard.Label>{item.title}</CheckboxCard.Label>
+                                    <CheckboxCard.Label>{criterio.titulo}</CheckboxCard.Label>
                                     <CheckboxCard.Description>
-                                        {item.description}
+                                        {criterio.descripcion}
                                     </CheckboxCard.Description>
                                 </CheckboxCard.Content>
                                 <CheckboxCard.Indicator />
                             </CheckboxCard.Control>
-                            {item.extra && (
+                            {criterio.items && (
                                 <CheckboxCard.Addon>
-                                    {restaurantes.map((restaurante) => (
-                                        <HStack justifyContent='space-between'>
-                                            <Stack key={restaurante.id} align='center'>
-                                                <Heading as='h2'>{restaurante.nombre}</Heading>
-                                                <Text>{restaurante.detalles}</Text>
-                                            </Stack>
-                                            <IconButton variant='ghost'><MdClose/></IconButton>
-                                        </HStack>
+                                    {criterio.items.map((item) => (
+                                        <ItemRow titulo={item.nombre} subtitulo={item.detalles} id={item.id} icono={<MdClose/>} />
                                     ))}
                                 </CheckboxCard.Addon>
                             )}    
                         </CheckboxCard.Root>
                     ))}
                 </Stack>
-                {/* <CheckboxCard.Root key='fiel' value='fiel' >
-                    <CheckboxCard.HiddenInput />
-                    <CheckboxCard.Control >
-                        <CheckboxCard.Content>
-                            <CheckboxCard.Label>Fieles</CheckboxCard.Label>
-                            <CheckboxCard.Description>
-                                Sólo los restaurantes preferidos
-                            </CheckboxCard.Description>
-                        </CheckboxCard.Content>
-                        <CheckboxCard.Indicator />
-                    </CheckboxCard.Control>
-                    <CheckboxCard.Addon>
-                        <HStack gap={3}>
-
-                        </HStack>
-                    </CheckboxCard.Addon>
-                </CheckboxCard.Root> */}
             </CheckboxGroup>
 
             <Button>Guardar</Button>
