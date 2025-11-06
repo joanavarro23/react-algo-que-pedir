e871597ef397dde1c3aedb372af4bdc1cda05adf
solalcaraz <alcarazmsol@gmail.com>
2025-11-06 12:02:42 -0300

    actualizacion de la ruta + ingredientes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,8 +1,8 @@
 // RUTAS DE LA PAGINA [React Routes]
 import { CheckoutPedido } from '@/pages/checkout-pedido/Checkout'
 import { PerfilUsuario } from '@/pages/usuario/Perfil'
-import { CriteriosBusqueda } from '@/pages/usuario/preferencias/criteriosBusqueda'
-import { IngredientesEvitar, IngredientesPreferidos } from '@/pages/usuario/preferencias/ingredientes'
+import { CriteriosBusqueda } from '@/pages/usuario/preferencias/CriteriosBusqueda'
+import { IngredientesEvitar, IngredientesPreferidos } from '@/pages/usuario/preferencias/Ingredientes'
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 import { AppLayout } from '@/components/layout/AppLayout'
 import { DetalleLocal } from '@/pages/detalle-local/DetalleLocal'
