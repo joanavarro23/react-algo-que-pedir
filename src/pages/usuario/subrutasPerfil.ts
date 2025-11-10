60e2100bc7417f795aa1082f14a19fc74055b6b8
solalcaraz <alcarazmsol@gmail.com>
2025-11-10 18:15:04 -0300

    separo tipado de Prefrencias

--- src/pages/usuario/subrutasPerfil.ts
+++ src/pages/usuario/subrutasPerfil.ts
@@ -1,8 +1,11 @@
-export const preferencias: {
+export type Preferencias = {
+    vista: string
     label: string
     path: '/criterios-busqueda' | '/ingredientes-preferidos' | '/ingredientes-evitar'
-}[] = [
-    { label: 'Criterios de búsqueda', path: '/criterios-busqueda' },
-    { label: 'Ingredientes preferidos', path: '/ingredientes-preferidos' },
-    { label: 'Ingredientes a evitar', path: '/ingredientes-evitar' },
+}
+
+export const preferencias: Preferencias[] = [
+    { vista: 'busqueda', label: 'Criterios de búsqueda', path: '/criterios-busqueda' },
+    { vista: 'preferidos', label: 'Ingredientes preferidos', path: '/ingredientes-preferidos' },
+    { vista: 'evitar', label: 'Ingredientes a evitar', path: '/ingredientes-evitar' },
 ]
