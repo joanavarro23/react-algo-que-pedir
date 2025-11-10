53acc63165870790206dec4401020fc17e69077d
carlasolrocca <carlasolrocca@gmail.com>
2025-11-10 20:26:20 -0300

    De nuevo agrego username y pass para que no rompa vista de Usuario

--- src/mocks/usuariosMock.ts
+++ src/mocks/usuariosMock.ts
@@ -1,7 +1,7 @@
 import { Usuario } from '@/domain/Usuario'
 
 export const USUARIOS_MOCK: Usuario[] = [
-    new Usuario('Pepita', 'Rodriguez', 'correo-pepita@gmail.com', 'Calle Falsa', 'Disneylandia', -3.3, 58.2),
-    new Usuario('Mona', 'Gimenez', 'mismo-correo@gmail.com', 'Siempre Vivo', 'Falopita', -34.61, -58.3),
-    new Usuario('Lucas', 'Martínez', 'mismo-correo@gmail.com', 'Avenida Cerca', 'Rosario', 2.95, -60.66),
+    new Usuario('Pepita', 'Rodriguez', '', '', 'correo-pepita@gmail.com', 'Calle Falsa', 'Disneylandia', -3.3, 58.2),
+    new Usuario('Mona', 'Gimenez', '', '', 'mismo-correo@gmail.com', 'Siempre Vivo', 'Falopita', -34.61, -58.3),
+    new Usuario('Lucas', 'Martínez', '', '', 'mismo-correo@gmail.com', 'Avenida Cerca', 'Rosario', 2.95, -60.66),
 ]
