05f33d91d5bc2398768d5a9eff85fde2ce54fe09
solalcaraz <alcarazmsol@gmail.com>
2025-11-10 11:28:43 -0300

    agrego mensaje de error al strategy

--- src/utils/validacionStrategy.ts
+++ src/utils/validacionStrategy.ts
@@ -1,28 +1,54 @@
+export type ValidacionResultado = {
+    esValido: boolean,
+    mensajeError: string
+}
 interface ValidacionStrategy<T> {
-    esValido(valor: T, rango?: { min: number, max: number }): boolean
+    esValido(valor: T, rango?: { min: number, max: number }): ValidacionResultado
+}
+
+// Validacion compuesta
+class CompositeValidacion implements ValidacionStrategy<string | number> {
+    esValido(valor: string | number, rango?: { min: number; max: number }): ValidacionResultado {
+        return {
+            esValido: true,
+            mensajeError: ''
+        }
+    }
 }
 
 class TextoRequerido implements ValidacionStrategy<string> {
-    esValido(valor: string): boolean {
-        return (!valor || valor.trim().length === 0)
+    esValido(valor: string): ValidacionResultado {
+        return {
+            esValido: (!valor || valor.trim().length === 0),
+            mensajeError: `El campo: ${valor} es un campo requerido`
+        }
     }
 }
 
 class ValorRequerido implements ValidacionStrategy<number> {
-    esValido(valor: number): boolean {
-        return !valor
+    esValido(valor: number): ValidacionResultado {
+        return {
+            esValido: !isNaN(valor),
+            mensajeError: `El campo: ${valor} es un campo requerido`
+        }
     }
 }
 
 class ValorPositivo implements ValidacionStrategy<number> {
-    esValido(valor: number): boolean {
-        return valor <= 0
+    esValido(valor: number): ValidacionResultado {
+        return {
+            esValido: valor <= 0,
+            mensajeError: 'Debe ser un número positivo'
+        }
     }
 }
 
 class RangoNumerico implements ValidacionStrategy<number> {
-    esValido(valor: number, rango: { min: number, max: number }): boolean {
-        return valor < rango.min || valor > rango.max
+    esValido(valor: number, rango: { min: number, max: number }): ValidacionResultado {
+        return {
+            esValido: valor < rango.min || valor > rango.max,
+            mensajeError: `El campo: ${valor} debe estar entre ${rango.min} y ${rango.max}`
+        }
     }
 }
 
