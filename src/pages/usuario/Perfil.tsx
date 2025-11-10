5e229635d6f365f073cdd70a056c52635c26d5b5
solalcaraz <alcarazmsol@gmail.com>
2025-11-10 18:15:49 -0300

    manejo de vistas segun cual sea

--- src/pages/usuario/Perfil.tsx
+++ src/pages/usuario/Perfil.tsx
@@ -1,17 +1,16 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 import { Button } from '@/components/boton/boton'
 import { toaster } from '@/components/chakra-toaster/toaster'
 import { useOnInit } from '@/customHooks/useOnInit'
 import { Usuario } from '@/domain/Usuario'
 import { usuarioService } from '@/services/usuarioService'
 import { getMensajeError } from '@/utils/errorHandling'
-import { Avatar, Card, Heading, HStack, IconButton, Stack, Text, VStack } from '@chakra-ui/react'
+import { Avatar, Heading, Stack, Text, VStack } from '@chakra-ui/react'
 import { useState } from 'react'
-import { FaChevronRight } from 'react-icons/fa'
 import { useNavigate, type ErrorResponse } from 'react-router-dom'
-import { preferencias } from './preferencias/rutas'
-import { useValidacion } from '@/customHooks/useValidacion'
-import { CampoTexto } from '@/components/label-input/CampoTexto'
+import { InformacionPersonal } from './formulario/InformacionPersonal'
+import { CriteriosBusqueda } from './preferencias/CriteriosBusqueda'
+import { IngredientesEvitar, IngredientesPreferidos } from './preferencias/Ingredientes'
+import type { Preferencias } from './subrutasPerfil'
 
 export const PerfilUsuario = () => {
     const imagen = '/usuario-chica.png'
@@ -33,10 +32,6 @@ export const PerfilUsuario = () => {
         }
     }
     useOnInit(traerUsuario) 
-
-    // Validaciones compuestas para los numeros:
-    const latitudValidacion = useValidacion(usuario.latitud, 'valorRequerido') || useValidacion(usuario.latitud, 'rangoNumerido', {min: -90, max: 90})
-    const longitudValidacion = useValidacion(usuario.longitud, 'valorRequerido') || useValidacion(usuario.longitud, 'rangoNumerido', {min: -180, max: 180})
 
     // Actualización de los campos inputs
     const actualizar = (referencia: keyof typeof usuario, valor: unknown) => {
@@ -65,10 +60,26 @@ export const PerfilUsuario = () => {
 
     // Navegación a las preferencias
     const navigate = useNavigate()
-    const gotoPreferencias = (opcion: { label: string; path: string }) => {
+    const gotoPreferencias = (opcion: Preferencias) => {
+        setVistaActual(opcion.vista)
         navigate(opcion.path)
     }
 
+    // Manejo de los datos entre la navegacion
+    const [vistaActual, setVistaActual] = useState<string>('form')
+    const mostrarVista = () => {
+        switch (vistaActual) {
+            case 'form':
+                return <InformacionPersonal data={usuario} actualizar={actualizar} navegacion={gotoPreferencias}/> 
+            case 'busqueda':
+                return <CriteriosBusqueda/>
+            case 'preferidos':
+                return <IngredientesPreferidos/>
+            case 'evitar':
+                return <IngredientesEvitar/>
+        }
+    }
+    
     return(
         <Stack py='5'>
            <Heading as='h1' size='md' textAlign="center">Perfil</Heading>
@@ -81,54 +92,7 @@ export const PerfilUsuario = () => {
                 <Text color='textoSecundario'>{usuario.email}</Text>
            </VStack>
 
-           {/* Informacion personal */}
-           <Card.Root variant='outline'>
-                <Card.Header>
-                    <Card.Title>Informacion Personal</Card.Title>
-                </Card.Header>
-                <Card.Body>
-                    <Stack gap='4'>
-                        <CampoTexto invalid={useValidacion(usuario.nombre, 'textoRequerido')} nombreLabel='Nombre' nombreTest='nombre' placeholder='Nombre'
-                        value={usuario.nombre} onChange={(event) => actualizar('nombre', event.target.value)} msjError='El campo nombre es requerido' />
-
-                        <CampoTexto invalid={useValidacion(usuario.apellido, 'textoRequerido')} nombreLabel='Apellido' nombreTest='apellido' placeholder='Apellido'
-                        value={usuario.apellido} onChange={(event) => actualizar('apellido', event.target.value)} msjError='El campo apellido es requerido' />
-                        
-                        <CampoTexto invalid={useValidacion(usuario.direccion, 'textoRequerido')} nombreLabel='Direccion' nombreTest='direccion' placeholder='Direccion'
-                        value={usuario.direccion} onChange={(event) => actualizar('direccion', event.target.value)} msjError='El campo dirección es requerido' />
-
-                        <CampoTexto invalid={useValidacion(usuario.ubicacion, 'textoRequerido')} nombreLabel='Ubicación' nombreTest='ubicacion' placeholder='Ciudad, Provincia'
-                        value={usuario.ubicacion} onChange={(event) => actualizar('ubicacion', event.target.value)} msjError='El campo ubicación es requerido' />
-
-                        <Stack direction='row'>
-                            <CampoTexto invalid={latitudValidacion} nombreLabel='Latitud' nombreTest='latitud' placeholder='Ej: -34.61' type='number'
-                            value={usuario.latitud} onChange={(event) => actualizar('latitud', event.target.value)} msjError='El campo latitud es requerido y debe estar entre -90 y 90' />
-                            
-                            <CampoTexto invalid={longitudValidacion} nombreLabel='Longitud' nombreTest='longitud' placeholder='Ej: 58.38' type='number'
-                            value={usuario.longitud} onChange={(event) => actualizar('longitud', event.target.value)} msjError='El campo longitud es requerido y debe estar entre -180 y 180' />
-                        </Stack>
-                    </Stack>
-                </Card.Body>
-           </Card.Root>
-
-           {/* Preferencias */}
-           <Card.Root variant='outline'>
-                <Card.Header>
-                    <Card.Title>Preferencias</Card.Title>
-                </Card.Header>
-                <Card.Body>
-                    <Stack>
-                        {preferencias.map((opcion) => (
-                            <HStack justify="space-between" align="center"
-                            key={opcion.path} onClick={(() => gotoPreferencias(opcion))}>
-                                <Text>{opcion.label}</Text>
-                                <IconButton variant="ghost" size="sm"
-                                ><FaChevronRight /></IconButton>
-                            </HStack>
-                        ))}
-                    </Stack>
-                </Card.Body>
-           </Card.Root>
+            {mostrarVista()}
 
            <Button onClick={guardar}>Guardar Cambios</Button>
         </Stack>
