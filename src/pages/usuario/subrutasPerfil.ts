7d45f71c13dbc446ff7b4f7dfeccaa461233029a
solalcaraz <alcarazmsol@gmail.com>
2025-11-10 22:03:36 -0300

    agrego subrutas

--- src/pages/usuario/subrutasPerfil.ts
+++ src/pages/usuario/subrutasPerfil.ts
@@ -1,11 +1,11 @@
 export type Preferencias = {
     vista: string
     label: string
-    path: '/criterios-busqueda' | '/ingredientes-preferidos' | '/ingredientes-evitar'
+    path: 'criterios-busqueda' | 'ingredientes-preferidos' | 'ingredientes-evitar'
 }
 
 export const preferencias: Preferencias[] = [
-    { vista: 'busqueda', label: 'Criterios de búsqueda', path: '/criterios-busqueda' },
-    { vista: 'preferidos', label: 'Ingredientes preferidos', path: '/ingredientes-preferidos' },
-    { vista: 'evitar', label: 'Ingredientes a evitar', path: '/ingredientes-evitar' },
+    { vista: 'busqueda', label: 'Criterios de búsqueda', path: 'criterios-busqueda' },
+    { vista: 'preferidos', label: 'Ingredientes preferidos', path: 'ingredientes-preferidos' },
+    { vista: 'evitar', label: 'Ingredientes a evitar', path: 'ingredientes-evitar' },
 ]
