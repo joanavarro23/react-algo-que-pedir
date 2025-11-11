da64e4ad08df66da6ea4c99515884bdb8e6a55f1
solalcaraz <alcarazmsol@gmail.com>
2025-11-10 22:02:12 -0300

    ruteo del perfil

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,20 +1,23 @@
 // RUTAS DE LA PAGINA [React Routes]
 import { CheckoutPedido } from '@/pages/checkout-pedido/Checkout'
 import { PerfilUsuario } from '@/pages/usuario/Perfil'
-import { CriteriosBusqueda } from '@/pages/usuario/preferencias/criteriosBusqueda'
+import { CriteriosBusqueda } from '@/pages/usuario/preferencias/CriteriosBusqueda'
 import { IngredientesEvitar, IngredientesPreferidos } from '@/pages/usuario/preferencias/Ingredientes'
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 import { AppLayout } from '@/components/layout/AppLayout'
 import { DetalleLocal } from '@/pages/detalle-local/DetalleLocal'
+import { InformacionPersonal } from '@/pages/usuario/formulario/InformacionPersonal'
 
 export const AQPRoutes = () =>
     <Routes>
         {/* El layout envuelve al resto de las paginas*/ }
         <Route path="/" element={ <AppLayout /> }>
-            <Route path="/perfil-usuario" element={ <PerfilUsuario/> }> {/* ACA IRIAN LAS SUBRUTAS DEL PERFIL */}</Route>
-            <Route path="criterios-busqueda" element={<CriteriosBusqueda />} />
-            <Route path="ingredientes-preferidos" element={<IngredientesPreferidos />} />
-            <Route path="ingredientes-evitar" element={<IngredientesEvitar />} />
+            <Route path="perfil-usuario" element={ <PerfilUsuario/> }>
+                <Route path='perfil-usuario' element={<InformacionPersonal />} />
+                <Route path="criterios-busqueda" element={<CriteriosBusqueda />} />
+                <Route path="ingredientes-preferidos" element={<IngredientesPreferidos />} />
+                <Route path="ingredientes-evitar" element={<IngredientesEvitar />} />
+            </Route>
             <Route path="/checkout-pedido" element={<CheckoutPedido/>}> </Route>
             <Route path="/crear-pedido" element={<DetalleLocal/>} />
         </Route>
