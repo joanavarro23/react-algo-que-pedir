ed9922a11eec6a095e91b60a2c6c741031b47ad0
carlasolrocca <carlasolrocca@gmail.com>
2025-11-10 22:19:04 -0300

    Enters para que sea mas legible

--- src/components/card-plato/cardPlato.tsx
+++ src/components/card-plato/cardPlato.tsx
@@ -11,10 +11,13 @@ export const CardPlato = ( {plato} : CardPlatoProps ) => {
             
             <VStack align="flex-start" w="70%">
                 {plato.popular && <Tag.Root size="sm" color="parrafos" bg="none" variant="subtle" p="0"><Tag.Label>Popular</Tag.Label></Tag.Root>}
+                
                 <Heading as="h1" size="md" fontWeight="semibold"> {plato.nombre} </Heading>
+                
                 <Text textStyle="xs" fontWeight="medium" color="parrafos">
                 {plato.descripcion}
                 </Text>
+                
                 <Text textStyle="xs" fontWeight="semibold" bgColor="#fcebebff" borderRadius="md" p="1" px="3.5">${plato.precio.toFixed(2)}</Text>
             </VStack>
 
