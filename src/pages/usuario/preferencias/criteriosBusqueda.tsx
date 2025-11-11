487979f4c9aa319a26a7bb36d0064a82a561fc43
solalcaraz <alcarazmsol@gmail.com>
2025-11-11 15:46:18 -0300

    probando como funciona contador e restauranteRow

--- src/pages/usuario/preferencias/criteriosBusqueda.tsx
+++ src/pages/usuario/preferencias/criteriosBusqueda.tsx
@@ -1,13 +1,15 @@
 import { Button } from '@/components/boton/boton'
 import { ItemRow } from '@/components/itemRow/itemRow'
 import { CRITERIOS_MOCK } from '@/pages/usuario/preferencias/criterios'
-import { CheckboxCard, CheckboxGroup, Collapsible, Heading, HStack, IconButton, Stack } from '@chakra-ui/react'
+import { CheckboxCard, CheckboxGroup, Collapsible, Heading, HStack, IconButton, Stack, Text } from '@chakra-ui/react'
 import { IoMdArrowBack } from 'react-icons/io'
 import { MdClose } from 'react-icons/md'
 import type { PerfilContextType } from '../Perfil'
 import { useOutletContext } from 'react-router-dom'
 import { useState } from 'react'
 import { toaster } from '@/components/chakra-toaster/toaster'
+import { RestaurenteItem } from '@/components/perfil-usuario/restauranteItem'
+import { Contador } from '@/components/contador/contador'
 
 export const CriteriosBusqueda = () => {
     const { usuario, setTareas, traerUsuario, actualizar, navigate } = useOutletContext<PerfilContextType>()
@@ -59,18 +61,33 @@ export const CriteriosBusqueda = () => {
                                 </CheckboxCard.Content>
                                 <CheckboxCard.Indicator />
                             </CheckboxCard.Control>
-                            {criterio.items && (
+                            {criterio.items && criterio.type === 'restaurantes' && (
                                 <Collapsible.Root open={criterio.checked}>
                                     <Collapsible.Trigger display='none'></Collapsible.Trigger> {/* es necesario para el collapsible */}
                                     <Collapsible.Content>
                                         <CheckboxCard.Addon>
                                             {criterio.items.map((item) => (
-                                                <ItemRow titulo={item.nombre} subtitulo={item.tiempo} id={item.id} icono={<MdClose/>} />
+                                                <RestaurenteItem id={item.id} nombre={item.nombre} puntuacion={item.puntuacion} tiempo={item.tiempo} precio={item.precio} />
+                                                // <ItemRow titulo={item.nombre} subtitulo={item.tiempo} id={item.id} icono={<MdClose/>} />
                                             ))}
                                         </CheckboxCard.Addon>
                                     </Collapsible.Content>
                                 </Collapsible.Root>
-                            )}    
+                            )}
+                            {criterio.type === 'distancia' && (
+                                <Collapsible.Root open={criterio.checked}>
+                                    <Collapsible.Trigger display='none'></Collapsible.Trigger> {/* es necesario para el collapsible */}
+                                    <Collapsible.Content>
+                                        <CheckboxCard.Addon>
+                                            <HStack justifyContent='space-between'>
+                                                <Text>Distancia</Text>
+                                                <Contador valor={10} />
+                                            </HStack>
+                                        </CheckboxCard.Addon>
+                                    </Collapsible.Content>
+                                </Collapsible.Root>
+                            )}
+
                         </CheckboxCard.Root>
                     ))}
                 </Stack>
