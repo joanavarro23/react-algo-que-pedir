88f147b0d42af9989b8e3416baef6aaadc08ff88
carlasolrocca <carlasolrocca@gmail.com>
2025-11-13 16:46:29 -0300

    Creo archivo para clase Plato

---
+++ src/domain/Plato.ts
@@ -0,0 +1,10 @@
+
+//El back va a devolver esto
+export type PlatoJSON = {
+    id: number,
+    nombre: string,
+    descripcion: string,
+    imagenUrl: string,
+    precioUnitario: number,
+    popular?: boolean
+}
