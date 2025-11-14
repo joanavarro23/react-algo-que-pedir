e07d72284ca49712bd93c1a9d11b30e4402f78e8
carlasolrocca <carlasolrocca@gmail.com>
2025-11-14 12:49:13 -0300

    Creo common.ts con getAxiosData() + archivo localServiceTest solo para probar momentaneamente

--- src/services/localServiceTest.ts
+++ src/services/localServiceTest.ts
@@ -1,7 +1,10 @@
 //archivo momentaneo para poder laburar la vista 
 // del detalle-local
 import axios from 'axios'
+import type { AxiosResponse } from 'axios'
 import { REST_SERVER_URL } from './constants'
+import { getAxiosData } from './common'
+
 
 export type LocalDTO = {
 
