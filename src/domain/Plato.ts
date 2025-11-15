ec7d9ac024119a3aadaa1ddd0c09aa10fb7f6c32
carlasolrocca <carlasolrocca@gmail.com>
2025-11-14 23:45:01 -0300

    Armo clase Plato con manejo de url de img del backend

--- src/domain/Plato.ts
+++ src/domain/Plato.ts
@@ -1,3 +1,4 @@
+import { REST_SERVER_URL } from '@/services/constants'
 
 //El back va a devolver esto
 export type PlatoJSON = {
@@ -6,9 +7,27 @@ export type PlatoJSON = {
     descripcion: string,
     imagenUrl: string,
     precioUnitario: number,
-    popular?: boolean
+    popular: boolean
 }
 
 export class Plato {
-    
+    id: number
+    nombre: string
+    descripcion: string
+    imagenUrl: string
+    precioUnitario: number
+    popular: boolean
+
+    constructor(data : PlatoJSON) {
+        this.id = data.id
+        this.nombre = data.nombre
+        this.descripcion = data.descripcion
+        this.imagenUrl = data.imagenUrl ? `${REST_SERVER_URL}/${data.imagenUrl}` : './pizza-margherita.png'
+        this.precioUnitario = data.precioUnitario
+        this.popular = data.popular
+    }
+
+    static fromJSON(platoJSON : PlatoJSON) : Plato {
+        return new Plato(platoJSON)
+    }
 }
