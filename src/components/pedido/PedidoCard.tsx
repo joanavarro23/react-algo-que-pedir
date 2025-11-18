9a6aebbd2258bd43a3ad4c31d95e2efb52afdf95
David Pazos <david_xen06@hotmail.com>
2025-11-18 03:25:21 -0300

    Agrego funcionalidad de modal/drawer de confirmación, para que el usuario tenga que confirmar la acción antes de cancelar un pedido

--- src/components/pedido/PedidoCard.tsx
+++ src/components/pedido/PedidoCard.tsx
@@ -1,7 +1,9 @@
 import { LuX } from "react-icons/lu" 
 import { useNavigate } from "react-router-dom"
 import type { Pedido } from "@/pages/detalle-pedido/Pedido"
+import { useConfirmModal } from "@/customHooks/useConfirmModal"
 import { Button, Card, Image, Grid, GridItem, VStack } from "@chakra-ui/react"
+import { ConfirmDrawer } from "../confirm-modal/ConfirmModal"
 
 interface PedidoCardProps {
   order: Pedido
@@ -9,52 +11,83 @@ interface PedidoCardProps {
 }
 
 export const PedidoCard = ({ order, onCancel }: PedidoCardProps) => {
+  const navigate = useNavigate()
 
-  const navigate = useNavigate()  
+  const { isOpen, ask, confirm, cancel } = useConfirmModal()
+
   const clickearCard = () => {
     navigate(`/detalle-pedido/${order.id}`, { state: { pedido: order } })
-    }
+  }
 
   const cancelarPedido = (e: React.MouseEvent) => {
-      e.stopPropagation() 
-      onCancel(order.id)
-    }
+    e.stopPropagation()
+    ask(() => onCancel(order.id))
+  }
 
   return (
-    <Card.Root
-      w="100%"
-      variant = "elevated" //"falopa" //Si le paso un valor que no existe, queda como yo quiero. "Undefined" no funciona.
-      overflow="hidden"
-    >
-      <Card.Body p={0}>
-        
-        <Grid templateColumns="100px 1fr auto" templateRows="auto auto auto" w="100%" onClick={clickearCard} _hover={{ cursor: "pointer", bg: "gray.50" }}>
+    <>
+      <Card.Root
+        w="100%"
+        variant="elevated"
+        overflow="hidden"
+      >
+        <Card.Body p={0}>
+          <Grid
+            templateColumns="100px 1fr auto"
+            templateRows="auto auto auto"
+            w="100%"
+            onClick={clickearCard}
+            _hover={{ cursor: "pointer", bg: "gray.50" }}
+          >
+            <GridItem gridRow="1 / 4" gridColumn="1 / 2">
+              <Image
+                src={order.local.urlImagenLocal}
+                alt={order.local.nombre}
+                width="100px"
+                height="100%"
+                objectFit="fill"
+              />
+            </GridItem>
 
-          <GridItem gridRow="1 / 4" gridColumn="1 / 2">
-            <Image src={order.local.urlImagenLocal} alt={order.local.nombre} width="100px" height="100%" objectFit="fill"
-            //No me gusta cómo queda esto, porque hay que hacer que la imagen encaje perfecto con el tamaño del grid. Después lo vemos.
-            />
-          </GridItem>
+            <GridItem gridRow="1 / 4" gridColumn="2 / 3" p={2} display="flex" alignItems="center">
+              <VStack align="start" gap={0}>
+                <Card.Title fontSize="sm" mb={0}>
+                  {order.local.nombre}
+                </Card.Title>
+                <Card.Description fontSize="s" color="gray.600">
+                  Total: ${order.precioTotal.toFixed(2)}
+                </Card.Description>
+                <Card.Description fontSize="s" color="gray.600">
+                  {order.hora} - {order.items} artículos
+                </Card.Description>
+              </VStack>
+            </GridItem>
 
-          <GridItem gridRow="1 / 4" gridColumn="2 / 3" p={2} display="flex" alignItems="center">
-            <VStack align="start" gap={0}>
-              <Card.Title fontSize="sm" mb={0}> {order.local.nombre} </Card.Title>
-              <Card.Description fontSize="s" color="gray.600">Total: ${order.precioTotal.toFixed(2)} </Card.Description>
-              <Card.Description fontSize="s" color="gray.600">{order.hora} - {order.items} artículos </Card.Description>
-            </VStack>
-          </GridItem>
+            <GridItem gridRow="1 / 4" gridColumn="3 / 4" display="flex" alignItems="center" justifyContent="center" p={3}>
+              {order.estadoPedido !== "ENTREGADO" &&
+                order.estadoPedido !== "CANCELADO" && (
+                  <Button
+                    size="sm"
+                    variant="ghost"
+                    colorScheme="red"
+                    aria-label="Cancelar pedido"
+                    onClick={cancelarPedido}
+                  >
+                    <LuX size={20} />
+                  </Button>
+                )}
+            </GridItem>
+          </Grid>
+        </Card.Body>
+      </Card.Root>
 
-          <GridItem gridRow="1 / 4" gridColumn="3 / 4" display="flex" alignItems="center" justifyContent="center" p={3}>
-
-            {order.estadoPedido !== 'ENTREGADO' && order.estadoPedido !== 'CANCELADO' && (
-              <Button size="sm" variant="ghost" colorScheme="red" aria-label="Cancelar pedido" onClick={cancelarPedido}>
-                <LuX size={20} />
-              </Button>
-            )}
-          </GridItem>
-        </Grid>
-      </Card.Body>
-    </Card.Root>
+      <ConfirmDrawer
+        open={isOpen}
+        onConfirm={confirm}
+        onCancel={cancel}
+        message="¿Está seguro de que desea cancelar el pedido?"
+      />
+    </>
   )
 }
 
