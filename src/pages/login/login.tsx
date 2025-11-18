ce40a7b0b21c8702686dd570025d8178ed3782dc
carlasolrocca <carlasolrocca@gmail.com>
2025-11-18 12:35:14 -0300

    Agrego spinner + llamado al service en Login

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,47 +1,61 @@
 import { Stack, Heading, Text, Field, Input, Flex, Image, Link } from '@chakra-ui/react'
 import { useForm, Controller } from 'react-hook-form'
 import { Button } from '@/components/boton/boton'
-import { Link as RouterLink } from 'react-router-dom'
+import { Link as RouterLink, useNavigate } from 'react-router-dom'
 import { PasswordInput } from '@/components/ui/password-input'
+import { login } from '@/services/authService'
+import { LoadingSpinner } from '@/components/spinnerCargando/spinner'
+
+
 
 //Tipamos los inputs que va a recibir el form para el Login
 type LoginForm = {
-  username: string
+  usuario: string
   password: string
 }
 
 export const LoginUsuario = () => {
+  const navigate = useNavigate()
 
-    /*El useForm recibe default values*/
-    const { control, handleSubmit, formState: { isSubmitting } } = useForm<LoginForm>({
-        defaultValues: { username: '', password: '' },
-        mode: 'onTouched'
-    })
+  /*El useForm recibe default values*/
+  const { control, handleSubmit, formState: { isSubmitting } } = useForm<LoginForm>({
+    defaultValues: { usuario: '', password: '' },
+    mode: 'onTouched'
+  })
 
-    /*Simulamos una llamada para que no rompa el submit */
-    const onSubmit = async (data: LoginForm) => {
-        await new Promise((r) => setTimeout(r, 800))
-        console.log('login:', data)
-        // await authService.login(data.username, data.password)
+  /*Simulamos una llamada para que no rompa el submit */
+  const onSubmit = async (data: LoginForm) => {
+    try {
+      await login({
+        usuario: data.usuario,
+        password: data.password
+      })
+
+      navigate('/home')
+    } catch (error) {
+        console.error('ERROR - falta toast')
     }
+  }
+
+  if(isSubmitting) {return <LoadingSpinner/>} 
 
   return (
     <Stack p="3rem" gap="3" as="form" onSubmit={handleSubmit(onSubmit)}>
       <Flex justifyContent="center" alignItems="center" gap="2rem">
-        <Image src="./logo.svg" alt="Logo app" p="1" w="45px" h="45px" borderRadius="full" bgColor="red"/>
+        <Image src="./logo.svg" alt="Logo app" p="1" w="45px" h="45px" borderRadius="full" bgColor="red" />
         <Heading as="h1" size="3xl">Algo que pedir</Heading>
       </Flex>
 
       <Stack gap="5">
         {/* Usuario */}
         <Controller
-          name="username"
+          name="usuario"
           control={control}
           rules={{ required: 'El usuario es obligatorio' }}
           render={({ field, fieldState }) => (
             <Field.Root invalid={!!fieldState.error} required>
               <Field.Label>Usuario*</Field.Label>
-              <Input {...field}/>
+              <Input {...field} />
               <Field.ErrorText>{fieldState.error?.message}</Field.ErrorText>
             </Field.Root>
           )}
@@ -64,8 +78,8 @@ export const LoginUsuario = () => {
         />
 
         {/*El isSubmitting me deshabilita el boton cuando identifica que se ejecuto un onSubmit (evita envios duplicados) */}
-        <Button type="submit" disabled={isSubmitting} p="2" mt="1rem" borderRadius="full">
-          {isSubmitting ? 'Ingresando…' : 'Iniciar Sesión'}
+        <Button type="submit" p="2" mt="1rem" borderRadius="full">
+          Iniciar Sesión
         </Button>
 
         <Text textAlign="center">
