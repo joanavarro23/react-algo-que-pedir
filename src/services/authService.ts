79820885755bec03b865d744f705261ad7467788
carlasolrocca <carlasolrocca@gmail.com>
2025-11-18 12:35:36 -0300

    Armo fx para handleo de errores

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -9,13 +9,45 @@ type RegisterData = {
     confirmarPassword: string
 }
 
+type LoginData = {
+    usuario: string,
+    password: string
+}
+
 type AuthResponseUsuario = {
     success: boolean,
     message: string,
     usuario?: string
 }
 
-export const registro = async (data: RegisterData) : Promise<AuthResponseUsuario> => {
+export const login = async (data: LoginData): Promise<AuthResponseUsuario> => {
+    const url = `${REST_SERVER_URL}/api/login`
+
+    try {
+        const response = await axios.post<AuthResponseUsuario>(url, data)
+
+        //Si el back responde todo OK --> login exitoso con el setItem del Local Storage
+        if (response.data.success) {
+            if (response.data.usuario) {
+                localStorage.setItem('usuario', response.data.usuario)
+            }
+        }
+
+        toaster.create({
+            title: 'Bienvenido a Algo que Pedir',
+            description: `Hola, ${response.data.usuario}!`,
+            type: 'success',
+            duration: 3000
+        })
+
+        return response.data
+    } catch (error) {
+        handleAuthError(error, 'Error al iniciar sesión')       //Llama al handler de errores
+        throw error
+    }
+}
+
+export const registro = async (data: RegisterData): Promise<AuthResponseUsuario> => {
     const url = `${REST_SERVER_URL}/api/registro`
 
     try {
@@ -30,28 +62,30 @@ export const registro = async (data: RegisterData) : Promise<AuthResponseUsuario
 
         return response.data
 
-    } catch(error){
-        let errorMessage = 'Ocurrio un error inesperado'
-
-        if (axios.isAxiosError(error)) {
-            if (error.response) {
-                errorMessage = error.response.data.message || 'Error en la solicitud'
-            } else if (error.request) {
-                errorMessage = 'No se pudo conectar con el servidor. Intente más tarde.'
-            } else {
-                errorMessage = error.message
-            }
-        }
-
-        toaster.create({
-            title: 'Ocurrio un error!',
-            description: errorMessage,
-            type: 'error',
-            duration: 4000
-        })
-
-        throw new Error(errorMessage)
+    } catch (error) {
+        handleAuthError(error, 'Error en el registro')      //Llama al handler de errores
+        throw error
     }
 }
 
+//Auxiliar para handlear los errores de ambos casos
+const handleAuthError = (error: any, titulo: string) => {
+    let errorMessage = 'Ocurrio un error inesperado'
 
+    if (axios.isAxiosError(error)) {
+        if (error.response) {
+            errorMessage = error.response.data.message || 'Error en la solicitud'
+        } else if (error.request) {
+            errorMessage = 'No se pudo conectar con el servidor. Intente más tarde.'
+        } else {
+            errorMessage = error.message
+        }
+    }
+
+    toaster.create({
+        title: titulo,
+        description: errorMessage,
+        type: 'error',
+        duration: 4000
+    })
+}
