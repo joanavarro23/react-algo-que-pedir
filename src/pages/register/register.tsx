933e40e1043cebfbf9f4e1cecedfbb51ab4cbe95
carlasolrocca <carlasolrocca@gmail.com>
2025-11-17 23:31:37 -0300

    Armo manejo de errores en registro

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,16 +1,63 @@
 import { Image, Heading, Text, Flex, Stack, Link, Field, Input } from '@chakra-ui/react'
-import { useValidacion } from '@/customHooks/useValidacion'
 import { Usuario } from '@/domain/Usuario'
 import { useState } from 'react'
 import { Button } from '@/components/boton/boton'
-import { Link as RouterLink } from 'react-router-dom'
+import { Link as RouterLink, useNavigate } from 'react-router-dom'
 import { PasswordInput } from '@/components/ui/password-input'
+import React from 'react'
 
+//Tipado para los inputs
+type InputsRegistro = {
+    username: string,
+    password: string,
+    rePassword: string
+}
 
+//Tipado para objeto de errores con Partial de TS que hace ? a todos los atributos idem InputsRegistro
+type ErroresForm = Partial<InputsRegistro>
 
 export const RegisterUsuario = () => {
-    const [usuario, setUsuario] = useState<Usuario>(new Usuario())
+    const navigate = useNavigate()
 
+    //Estado para manejar las validaciones de los inputs
+    const [values, setValues] = useState<InputsRegistro>({
+        username: '',
+        password: '',
+        rePassword: ''
+    })
+
+    //El flag de enviado viene en false y el objeto de errores vacio x default 
+    const [submitted, setSubmitted] = useState(false)
+    const [errors, setErrors] = useState<ErroresForm>({})    
+
+    //Maneja los cambios en cualquier input del form
+    const manejoCambiosEnInput = (e : React.ChangeEvent<HTMLInputElement>, field: keyof InputsRegistro) => {
+        const  { value } = e.target
+        setValues( (datosPrevios) => ({
+            ...datosPrevios,
+            [field]: value
+        }) )
+    }
+
+    //Funcion para validar desde el front los requeridos y coincidencia
+    const validarFormulario = (data: InputsRegistro) : boolean => {
+        const posiblesErrores : ErroresForm = {}
+
+        if(!data.username) posiblesErrores.username = 'El usuario es obligatorio'
+        if(!data.password) posiblesErrores.password = 'La contraseña es obligatoria'
+        if(!data.rePassword) posiblesErrores.rePassword = 'Debe re-ingresar la contraseña'
+
+        if(data.password && data.rePassword && data.password !== data.rePassword) {
+            posiblesErrores.rePassword = 'Las contraseñas no coinciden'
+        }
+
+        setErrors(posiblesErrores)
+        return Object.keys(posiblesErrores).length === 0 //si no hay ningun error, retorna true
+    }
+
+    const guardarUsuario = (nuevoUsuario : Usuario) => {
+        
+    }
 
     return (
         <Stack p="3rem" gap="3" as="form">
@@ -21,19 +68,19 @@ export const RegisterUsuario = () => {
             </Flex>
 
             <Stack gap="5">
-                <Field.Root invalid>
+                <Field.Root required>
                     <Field.Label>Usuario*</Field.Label>
                     <Input placeholder="Ingresa tu usuario" />
                     <Field.ErrorText>El usuario es obligatorio</Field.ErrorText>
                 </Field.Root>
 
-                <Field.Root invalid required>
+                <Field.Root required>
                     <Field.Label>Password*</Field.Label>
                     <PasswordInput/>
                     <Field.ErrorText>La contraseña es obligatoria</Field.ErrorText>
                 </Field.Root>
 
-                 <Field.Root invalid required>
+                 <Field.Root required>
                     <Field.Label>Re-ingrese su Password*</Field.Label>
                     <PasswordInput/>
                     <Field.ErrorText>La contraseña es obligatoria</Field.ErrorText>
