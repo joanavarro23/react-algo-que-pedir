553d2bfadb4763542c0ef82f6c3f4356d44284c4
solalcaraz <alcarazmsol@gmail.com>
2025-11-20 13:49:16 -0300

    descomento axios para hacer la conexion al back

--- src/services/usuarioService.ts
+++ src/services/usuarioService.ts
@@ -1,21 +1,17 @@
 import axios from 'axios'
 import { REST_SERVER_URL } from './constants'
 import { Usuario, type UsuarioJSON } from '@/domain/Usuario'
-import { USUARIOS_MOCK } from '@/mocks/usuariosMock'
 
 const usuarioAsJson = (usuarioJSON: UsuarioJSON) => Usuario.fromJson(usuarioJSON)
 
 class UsuarioService {
     async getById(id: number) {
-        // const usuarioJson = await axios.get(`${REST_SERVER_URL}/usuario/${id}`)
-        // return usuarioAsJson(usuarioJson.data)
-        return USUARIOS_MOCK[id]
+        const usuarioJson = await axios.get(`${REST_SERVER_URL}/usuario/${id}`)
+        return usuarioAsJson(usuarioJson.data)
     }
 
     actualizar(usuarioActualizar: Usuario) {
-        //return axios.put(`${REST_SERVER_URL}/usuario/${usuario.id}`, usuario.toJSON())
-        const usuario = this.getById(usuarioActualizar.id!)
-        return { ...usuario, ...usuarioActualizar}
+        return axios.put(`${REST_SERVER_URL}/usuario/${usuarioActualizar.id}`, usuarioActualizar.toJSON())
     }
 }
 
