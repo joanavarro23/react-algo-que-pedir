657a96c22ca111f4087354fcd7f1e35ed5d854ef
solalcaraz <alcarazmsol@gmail.com>
2025-11-22 16:21:33 -0300

    refactor para el service del usuario

--- src/services/usuarioService.ts
+++ src/services/usuarioService.ts
@@ -1,17 +1,19 @@
 import axios from 'axios'
 import { REST_SERVER_URL } from './constants'
 import { Usuario, type UsuarioJSON } from '@/domain/Usuario'
-
-const usuarioAsJson = (usuarioJSON: UsuarioJSON) => Usuario.fromJson(usuarioJSON)
+import { getAxiosData } from './common'
 
 class UsuarioService {
     async getById(id: number) {
-        const usuarioJson = await axios.get(`${REST_SERVER_URL}/usuario/${id}`)
-        return usuarioAsJson(usuarioJson.data)
+        const queryById = () => axios.get<UsuarioJSON>(`${REST_SERVER_URL}/usuario/${id}`)
+        const usuarioJson = await getAxiosData(queryById)
+        return Usuario.fromJSON(usuarioJson)
     }
 
-    actualizar(usuarioActualizar: Usuario) {
-        return axios.put(`${REST_SERVER_URL}/usuario/${usuarioActualizar.id}`, usuarioActualizar.toJSON())
+    async actualizar(usuarioActualizar: Usuario) {
+        const query = () => axios.put(`${REST_SERVER_URL}/usuario/${usuarioActualizar.id}`, usuarioActualizar.toJSON())
+        const usuarioJson = await getAxiosData(query)
+        return Usuario.fromJSON(usuarioJson)
     }
 }
 
