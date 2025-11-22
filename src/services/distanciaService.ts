c4f61ad2a38bb82f36e9ba964bd26783be54e564
Joaquín Navarro <joanavarro23@hotmail.com>
2025-11-22 05:00:05 -0300

    arreglo error distancia

--- src/services/distanciaService.ts
+++ src/services/distanciaService.ts
@@ -1,19 +1,16 @@
 import axios from 'axios'
 import { REST_SERVER_URL } from './constants'
 
-interface DistanciaJSON {
-    distancia: string
-}
-
 class DistanciaService {
     async obtenerDistancia(localId: number, userId: number): Promise<string> {
-        const response = await axios.get<DistanciaJSON>(`${REST_SERVER_URL}/distancia`, {
+        const response = await axios.get<string>(`${REST_SERVER_URL}/distancia`, {
             params: {
                 fromLocalId: localId,
                 toUserId: userId,
             },
+            responseType: 'text',
         })
-        return response.data.distancia
+        return response.data
     }
 }
 
