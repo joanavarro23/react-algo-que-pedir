4c17367f960e2102b382ba5573530c4521b9c6f0
Joaquín Navarro <joanavarro23@hotmail.com>
2025-11-22 03:48:12 -0300

    agrego usuario a pedido

--- src/domain/Pedido.ts
+++ src/domain/Pedido.ts
@@ -2,6 +2,7 @@ import type { Carrito } from './Carrito'
 import { Plato } from './Plato'
 import { type PlatoJSON } from './Plato'
 import type { LocalJSON, MedioDePago } from '@/services/localServiceTest'
+import { Usuario, type UsuarioJSON } from './Usuario'
 
 export type PedidoJSON = {
     id: number
@@ -10,6 +11,7 @@ export type PedidoJSON = {
     medioDePago: MedioDePago
     costoTotalPedido: number,
     fechaPedido: String
+    usuario: UsuarioJSON
 }
 export class Pedido {
     id?: number
@@ -20,11 +22,13 @@ export class Pedido {
         public medioDePago: MedioDePago | null = null,
         public fechaPedido: Date | null = null,
         public costoTotalPedido = 0,
+        public usuario: Usuario | null = null
     ) {}
 
     static fromJson(pedidoJSON: PedidoJSON): Pedido {
        const pedido = Object.assign(new Pedido(), pedidoJSON, {
             platosDelPedido: pedidoJSON.platosDelPedido.map(platoJSON => Plato.fromJSON(platoJSON)),
+            usuario: Usuario.fromJson(pedidoJSON.usuario),
        })
        return pedido
     }
@@ -33,11 +37,12 @@ export class Pedido {
         carrito: Carrito,
         local: LocalJSON,
         medioDePago: MedioDePago,
-        costoTotalPedido: number,
-        fechaPedido: Date
+        costoTotalPedido: number, //Tengo que enviar esto para validar el pedido en el back que todavía no lo hice
+        fechaPedido: Date,
+        usuario: Usuario
     ): Pedido {
             const platosDelPedido = carrito.items.flatMap(item => Array(item.cantidad).fill(item.plato))
-            return new Pedido(local, platosDelPedido, medioDePago, fechaPedido, costoTotalPedido)
+            return new Pedido(local, platosDelPedido, medioDePago, fechaPedido, costoTotalPedido, usuario)
         }
         
         toJSON(): PedidoJSON {
@@ -50,7 +55,8 @@ export class Pedido {
                 platosDelPedido: this.platosDelPedido?.map(plato => plato.toJSON()) ?? [],
                 medioDePago: this.medioDePago ?? 'EFECTIVO',
                 costoTotalPedido: this.costoTotalPedido,
-                fechaPedido: this.fechaPedido.toISOString()
+                fechaPedido: this.fechaPedido.toISOString(),
+                usuario: this.usuario!.toJSON()
             }
         }
     }
