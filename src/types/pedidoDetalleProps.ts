dcd6627a224ec22432dcff1ede60e46e628945dd
carlasolrocca <carlasolrocca@gmail.com>
2025-11-25 18:22:37 -0300

    Arreglo tipado para usar Local de domain

--- src/types/pedidoDetalleProps.ts
+++ src/types/pedidoDetalleProps.ts
@@ -1,8 +1,8 @@
-import { type Restaurante } from './restaurante'
+import { Local } from '@/domain/Local'
 import { Articulo } from '@/components/articulo-checkout/Articulo'
 
 export interface PedidoDetalleProps {
-  restaurante: Restaurante
+  restaurante: Local
   articulos: Articulo[]
   subtotal: number
   recargo: number
