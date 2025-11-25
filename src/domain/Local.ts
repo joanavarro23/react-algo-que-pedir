d509fd9360fca1f9732b0b61a69a1faba8a2e8b5
solalcaraz <alcarazmsol@gmail.com>
2025-11-24 22:51:08 -0300

    actualizo a clase local con sus metodos

--- src/domain/Local.ts
+++ src/domain/Local.ts
@@ -6,6 +6,42 @@ export const medioDePagoLabels: Record<MedioDePago, string> = {
   QR: 'QR',
 }
 
+export class Local {
+  idLocal?: number
+
+  constructor(
+    public nombre: string = '',
+    public urlImagenLocal: string = '',
+    public mediosDePago: MedioDePago[] = [],
+    public rating: number = 0,
+    public reviews: string = '',
+    public tarifaEntrega: number = 0,
+    public recargosMedioDePago: Record<MedioDePago, number> = { EFECTIVO: 0, TARJETA: 0, QR: 0}
+  ){}
+
+  static fromJSON(localJSON: LocalCriterioJSON): Local {
+    return Object.assign(new Local(), localJSON)
+  }
+
+  toJSON(): LocalCriterioJSON {
+    return {
+      idLocal: this.idLocal!,
+      nombre: this.nombre,
+      urlImagenLocal: this.urlImagenLocal,
+      rating: this.rating,
+      tarifaEntrega: this.tarifaEntrega
+    }
+  }
+}
+
+export type LocalCriterioJSON = {
+  idLocal: number,
+  nombre: string,
+  urlImagenLocal: string,
+  rating: number,
+  tarifaEntrega: number,
+}
+
 export type LocalJSON = {
     idLocal: number,
     nombre: string,
