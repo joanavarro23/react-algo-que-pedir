ac419e72121f2ce1e22060fe3cc5dda5b689ebe3
solalcaraz <alcarazmsol@gmail.com>
2025-11-24 22:54:39 -0300

    agrego tarifaEntrega al componente local

--- src/components/perfil-usuario/restauranteItem.tsx
+++ src/components/perfil-usuario/restauranteItem.tsx
@@ -6,17 +6,18 @@ export type RestaurenteItemProps = {
     nombre: string
     imagen: string
     puntuacion: number
+    tarifaEntrega: number
     onEliminar: (id: number) => void
 }
 
-export const RestaurenteItem = ({ id, nombre, imagen ,puntuacion, onEliminar }: RestaurenteItemProps) => {
+export const RestaurenteItem = ({ id, nombre, imagen ,puntuacion, tarifaEntrega, onEliminar }: RestaurenteItemProps) => {
     return(
         <HStack justifyContent='space-between' key={id}>
             <HStack>
                 <Image src={imagen} objectFit='cover' overflow='hidden' boxSize='2.5rem'/>
                 <Stack>
                     <Text fontSize='md'>{nombre}</Text>
-                    <Text fontSize='sm'>{puntuacion} ⭐</Text>
+                    <Text fontSize='sm'>{puntuacion.toFixed(1)} ★ · {tarifaEntrega.toFixed(2)}</Text>
                 </Stack>
             </HStack>
             <IconButton variant='ghost' onClick={() => onEliminar(id)}> <MdClose/> </IconButton>
